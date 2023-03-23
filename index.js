fc7d7022e79d940055c08e2187cd6fc3d04a4ba5
fazal <fazal@lotusinsights.in>
2023-03-23 17:45:13 +0530

    rtk

---
+++ index.js
@@ -0,0 +1,17 @@
+const store = require('./app/store')
+const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
+
+console.log('Initial State' ,store.getState())
+
+const unsubscribe = store.subscribe(() => {
+    console.log('updated state', store.getState())
+})
+
+store.dispatch(fetchUsers())
+
+
+
+
+//unsubscribe()
